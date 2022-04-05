f94972571e778f80eb850f7ffafb7c723fca1cd2
FITRAH162 <s160419162@student.ubaya.ac.id>
2022-04-05 17:30:44 +0700

    mulia hati layout init

---
+++ app/src/main/java/com/ubaya/a160419162_muliahati/view/DetailDonasikuFragment.kt
@@ -0,0 +1,22 @@
+package com.ubaya.a160419162_muliahati.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ubaya.a160419162_muliahati.R
+
+
+class DetailDonasikuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_donasiku, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/ubaya/a160419162_muliahati/view/PenggalangDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.ubaya.a160419162_muliahati.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ubaya.a160419162_muliahati.R
+
+
+class PenggalangDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_penggalang_detail, container, false)
+    }
+
+}
