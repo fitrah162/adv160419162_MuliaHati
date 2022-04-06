4b7eaa2296ff7fab035e268eb69a4a5b0b8b5018
FITRAH162 <s160419162@student.ubaya.ac.id>
2022-04-06 16:35:12 +0700

    finish()

--- app/src/main/java/com/ubaya/a160419162_muliahati/view/MainActivity.kt
+++ app/src/main/java/com/ubaya/a160419162_muliahati/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ubaya.a160419162_muliahati.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -19,9 +20,25 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(navView, navController)
         bottomNav.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.itemHome -> showBottomNav()
+                R.id.itemProfile -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(drawerLayout) || super.onSupportNavigateUp()
+    }
+    private fun showBottomNav() {
+        bottomNav.visibility = View.VISIBLE
+
+    }
+
+    private fun hideBottomNav() {
+        bottomNav.visibility = View.GONE
+
     }
 }
